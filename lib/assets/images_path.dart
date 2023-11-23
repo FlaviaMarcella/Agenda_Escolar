dfbac4287f0ad0f023eb9f3cca89b946ff9d6e1d
FlaviaMarcella <129198398+FlaviaMarcella@users.noreply.github.com>
2023-11-23 14:41:58 -0300

    feat: create dependency and variáveis src

---
+++ lib/assets/images_path.dart
@@ -0,0 +1,6 @@
+class ImagePaths {
+  static const String calendarImage = 'lib/assets/images/calendar.png';
+  static const String linkBrokenImage = 'lib/assets/images/linkbroken.png';
+  static const String suapImage = 'lib/assets/images/iconSuap.png';
+  // Adicione outros caminhos de imagens conforme necessário
+}
