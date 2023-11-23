114d35733df74c65439dcb80091c77d0ee0038ff
FlaviaMarcella <129198398+FlaviaMarcella@users.noreply.github.com>
2023-11-23 13:28:44 -0300

    feat: add colors palet

---
+++ lib/assets/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const Color primaryColor = Color.fromRGBO(2, 115, 155,1);
+  static const Color secondaryColor = Color.fromRGBO(3, 140, 127,1);
+  static const Color accentColor = Color.fromRGBO(173, 217, 209,1);
+  static const Color detailsColor = Color.fromRGBO(217, 82, 82,1);
+  static const Color darkColor = Color.fromRGBO(12, 12, 12,1);
+  // Adicione outras cores conforme necessário
+}
