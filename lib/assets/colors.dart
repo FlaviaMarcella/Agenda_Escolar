da09216c6eed3b2ab2f48076c35eb87b6fd509a7
FlaviaMarcella <129198398+FlaviaMarcella@users.noreply.github.com>
2023-11-23 18:35:32 -0300

    feat: add config colors e vars

--- lib/assets/colors.dart
+++ lib/assets/colors.dart
@@ -4,7 +4,8 @@ class AppColors {
 
   static const Color primaryColor = Color.fromRGBO(2, 115, 155,1);
   static const Color secondaryColor = Color.fromRGBO(3, 140, 127,1);
-  static const Color accentColor = Color.fromRGBO(173, 217, 209,1);
+  static const Color accentColor = Color.fromRGBO(202, 230, 224, 1);
+  static const Color whiteColor = Color.fromRGBO(173, 217, 209,1);
   static const Color detailsColor = Color.fromRGBO(217, 82, 82,1);
   static const Color darkColor = Color.fromRGBO(12, 12, 12,1);
   // Adicione outras cores conforme necessário
--- lib/assets/images_path.dart
+++ lib/assets/images_path.dart
@@ -2,5 +2,6 @@ class ImagePaths {
   static const String calendarImage = 'lib/assets/images/calendar.png';
   static const String linkBrokenImage = 'lib/assets/images/linkbroken.png';
   static const String suapImage = 'lib/assets/images/iconSuap.png';
+  static const String avatarImage = 'lib/assets/images/avatar.png';
   // Adicione outros caminhos de imagens conforme necessário
 }
